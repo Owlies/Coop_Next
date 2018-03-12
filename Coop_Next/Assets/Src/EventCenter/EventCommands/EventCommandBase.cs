7a98e84c00381ee167410ae1c817cfc2f14de36f
Huayu <owlies.hwayu@gmail.com>
2018-03-11 21:00:53 -0700

    code architecture

---
+++ Coop_Next/Assets/Src/EventCenter/EventCommands/EventCommandBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract class EventCommandBase {
+    protected GameObject actor;
+    public abstract void Execute();
+    public EventCommandBase(GameObject actor) {
+        this.actor = actor;
+    }
+
+}
---
+++ Coop_Next/Assets/Src/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager> {
+    private EventCenter eventCenter;
+    private PlayerManager playerManager;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void InitializeServices() {
+        eventCenter = EventCenter.Instance;
+        playerManager = PlayerManager.Instance;
+    }
+
+}
