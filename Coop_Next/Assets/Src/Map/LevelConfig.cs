8886f3b441715d69cefd4b020bf141238cd8dde6
owlies.zihan@gmail.com <owlies.zihan@gmail.com>
2018-03-17 20:45:20 -0700

    add new scene

---
+++ Coop_Next/Assets/Src/Map/LevelConfig.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu(fileName = "Level", menuName = "CoopNext/Level", order = 1)]
+public class LevelConfig : ScriptableObject {
+
+}
