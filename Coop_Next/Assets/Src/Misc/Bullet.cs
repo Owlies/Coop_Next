4cc2604c791781c6e857b77ac34ec137cce13e21
Huayu Yang <owlies.hwayu@gmail.com>
2018-05-22 15:13:11 -0700

    arrow prefab

---
+++ Coop_Next/Assets/Src/Misc/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+	public GameObject target;
+
+	private float movingSpeed;
+	private float damage;
+	
+	public void Initialize(GameObject target, float speed, float damage) {
+		this.target = target;
+		this.movingSpeed = speed;
+		this.damage = damage;
+	}
+
+	/// <summary>
+	/// OnTriggerEnter is called when the Collider other enters the trigger.
+	/// </summary>
+	/// <param name="other">The other Collider involved in this collision.</param>
+	void OnTriggerEnter(Collider other)
+	{
+		
+	}
+}
