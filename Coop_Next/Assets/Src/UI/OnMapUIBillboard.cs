5eb8e9162f2082e74ae48e10e8a92c3bae18e53f
Huayu <owlies.hwayu@gmail.com>
2018-06-20 22:51:48 -0700

    UI billboard

---
+++ Coop_Next/Assets/Src/UI/OnMapUIBillboard.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnMapUIBillboard : OverridableMonoBehaviour {
+    Text title;
+    Text content;
+
+	// Use this for initialization
+	void Start () {
+        title = GetComponentsInChildren<Text>()[0];
+        content = GetComponentsInChildren<Text>()[1];
+    }
+
+    public void Initialize(string titleString, string contentString) {
+        UpdateTitle(titleString);
+        UpdateContent(contentString);
+    }
+
+    public void UpdateTitle(string titleString) {
+        title.text = titleString;
+    }
+
+    public void UpdateContent(string contentString) {
+        content.text = contentString;
+    }
+}
