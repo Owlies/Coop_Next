20a35218359c402f689c4cb34b54049ea06102f6
owlies.zihan <owlies.zihan@gmail.com>
2018-08-11 20:26:53 +0800

    add FlameThrower

--- Coop_Next/Assets/Src/Buildings/FlameThrower.cs
+++ Coop_Next/Assets/Src/Buildings/FlameThrower.cs
@@ -2,6 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlameThrower : BuildingBase {
+public class FlameThrower : AttackBuilding {
 
+    protected override bool TryAttackEnemy()
+    {
+        if (!CanAttackEnemy())
+        {
+            attackingEnemy = null;
+            return false;
+        }
+
+        foreach (EnemyBase enemy in EnemyManager.Instance.GetAllAliveEnemies())
+        {
+            if (Vector3.Distance(enemy.transform.position, this.transform.position) <= attackRange)
+            {
+                enemy.TakeDamage(attackDamage);
+            }
+        }
+
+        attackState = EAttackBuildingState.COOLING_DOWN;
+        attackCoolDownStartTime = Time.time;
+
+        return true;
+    }
+
+    protected override bool CanAttackEnemy()
+    {
+        if (attackState == EAttackBuildingState.COOLING_DOWN)
+        {
+            return false;
+        }
+
+        return true;
+    }
 }
