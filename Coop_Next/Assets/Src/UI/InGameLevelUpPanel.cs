932e83545cb1647f6904a9a432dabb6486e603cd
owlies.zihan <owlies.zihan@gmail.com>
2018-09-23 16:36:14 +0800

    level up panel part 1

---
+++ Coop_Next/Assets/Src/UI/InGameLevelUpPanel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameLevelUpPanel : MonoBehaviour {
+
+    public GameObject checkMark1;
+    public GameObject checkMark2;
+
+    public Button selectButton;
+
+    public Text techText1;
+    public Text techText2;
+
+    private string[] techName = new string[2];
+    private int currentIdx = 0;
+
+    public void InitPanel(string tech1, int tech1Level, string tech2, int tech2Level)
+    {
+        techText1.text = tech1 + "\n LV " + tech1Level;
+        techText2.text = tech2 + "\n LV " + tech2Level;
+        techName[0] = tech1;
+        techName[1] = tech2;
+    }
+
+    public void SelectTech(int idx)
+    {
+        currentIdx = idx;
+        if (idx == 0)
+        {
+            checkMark1.SetActive(true);
+            checkMark2.SetActive(false);
+        }
+        else if (idx == 1)
+        {
+            checkMark1.SetActive(false);
+            checkMark2.SetActive(true);
+        }
+    }
+
+    public void ConfirmSelection()
+    {
+
+    }
+}
